02f998b0138e2eb5796e45a0f06a457e34e2b2c3
Annie Hahn <annie.hahn@apexlearning.com>
2017-06-02 18:59:33 -0700

    get wiki images

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-var locations = [
+let locations = [
   {
     name:'Cliffs of Moher',
     lat: 52.9719,
@@ -49,14 +49,13 @@ var locations = [
     wikiContent: ""
   }]
 
-var filter;
-var currentLocations;
-var map;
-var markers = [];
-var infoWindow = null;
+let filter;
+let currentLocations;
+let map;
+let markers = [];
+let infoWindow = null;
 
 function initMap() {
-
     map = new google.maps.Map(document.getElementById('map'), {
       zoom: 11,
       center: {lat: 53.0160, lng: -9.355},
@@ -64,44 +63,39 @@ function initMap() {
     });
 
     // get wiki data and create marker for each location
-    for (var i = 0, l = locations.length; i < l; i++) {
-      getWikiData(locations[i]);
-      addMarker(locations[i]);
+    for (let i = 0, l = locations.length; i < l; i++) {
+      getWikiData(locations[i], addMarker);
     }
- }
+}
 
 function addMarker(location) {
-    var title = location.name;
-    var lat = location.lat;
-    var lng = location.lng;
-    var content = location.wikiContent;
+  let title = location.name;
+  let lat = location.lat;
+  let lng = location.lng;
+  let content = location.wikiContent;
 
-    var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, lng),
-      map: map,
-      title: title
-    });
+  let marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: map,
+    title: title
+  });
 
-    markers.push(marker);
+  markers.push(marker);
 
-    google.maps.event.addListener(marker,'click', (function (marker, content) { //?
-        return function () {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function(){ marker.setAnimation(null); }, 750);
-            showInfoWindow(marker, content);
-        }
-    })(marker, content));
+  marker.addListener('click', function() {
+    showInfoWindow(marker, content);
+  });
 }
 
 function mapViewModel() {
 
   filter = ko.observable("");
 
-  // Array of locations based on search input (case insensitive)
+  // filter list of locations based on search input (case insensitive)
   currentLocations = ko.computed(function() {
-    var filterTerm = filter().toLowerCase();
+    let filterTerm = filter().toLowerCase();
     if (!filterTerm) {
-      for (var i = 0, l = locations.length; i < l; i++) {
+      for (let i = 0, l = locations.length; i < l; i++) {
         locations[i].visible = true;
       }
       return locations;
@@ -118,17 +112,19 @@ function mapViewModel() {
     }
   }, mapViewModel);
 
+  // filtering locations should also filter markers on the map
   currentLocations.subscribe(function() {
     filterMarkers();
   });
 
+  // clicking on location name opens infowindow
   showLocation = function(location) {
     showInfoWindow(markers[location.index], location.wikiContent);
   }
 }
 
 function filterMarkers() {
-  for (i = 0; i < markers.length; i++) {
+  for (let i = 0; i < markers.length; i++) {
     marker = markers[i];
     if (locations[i].visible) {
       marker.setVisible(true);
@@ -149,21 +145,47 @@ function showInfoWindow(marker, content) {
   infoWindow.open(map, marker);
 }
 
-function getWikiData(location) {
-  var wikiurl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+function getWikiData(location, addMarker) {
+  let wikiSummaryUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
     location.name + "&format=json";
 
-  $.ajax({
-    url: wikiurl,
+  let wikiImageUrl = "https://en.wikipedia.org/w/api.php?action=query&formatversion=2" +
+    "&prop=pageimages&piprop=thumbnail&pithumbsize=250&titles=" +
+    location.name + "&format=json";
+
+  let imageSource;
+
+   $.ajax({
+    url: wikiSummaryUrl,
     dataType: "jsonp",
     success: function(response) {
       let wikiContent = response[2][0];
-      let contentString =
-      '<div id="content">'+
-        '<h1>' + location.name + '</h1>'+
-        '<p>' + wikiContent + '</p>'+
-      '</div>';
-      location.wikiContent = contentString;
+      // if wikipedia summary call is successful, get image
+      $.ajax({
+        url: wikiImageUrl,
+        dataType: "jsonp",
+        success: function(response) {
+          if(response.query.pages[0].hasOwnProperty("thumbnail") === true) {
+            imageSource = response.query.pages[0].thumbnail.source;
+
+          let contentString =
+          '<div>'+
+            '<h1>' + location.name + '</h1>'+
+            '<p>' + wikiContent + '</p>'+
+            '<div class="image-container">' +
+              '<img class="image" src='+ imageSource +'>' +
+            '</div>' +
+          '</div>';
+
+          location.wikiContent = contentString;
+          }
+
+          addMarker(location);
+        },
+        error: function() {
+        alert("An error occurred loading Wikipedia images. Please try again.");
+        }
+      });
     },
     error: function() {
       alert("An error occurred loading Wikipedia content. Please try again.");
